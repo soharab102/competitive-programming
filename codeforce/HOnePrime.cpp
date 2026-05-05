8edd8d4a8fff5f6103451c47a8b869d15cc9e028
soharab102 <hossainsoharab102@gmail.com>
2026-05-06 03:36:46 +0600

    solve prime number problem

--- codeforce/HOnePrime.cpp
+++ codeforce/HOnePrime.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-int main()
-{
-    cout<<"Hello";
+
+void primeCheck(int X){
+    if(X <= 1){
+        cout << "NO\n";
+        return;
+    }
+
+    for(int i = 2; i <= sqrt(X); i++){
+        if(X % i == 0){
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    cout << "YES\n";
+}
+
+int main(){
+    int X;
+    cin >> X;
+    primeCheck(X);
+    return 0;
 }
